548c7d488f8b87c890b0aa372984f8e53f2616c3
Habibullah02 <habibullah.mahbobi@gmail.com>
2022-09-30 18:16:35 -0400

    simple basic project

---
+++ src/class03A14/Class03.java
@@ -0,0 +1,10 @@
+package class03A14;
+
+public class Class03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println(" this is new class");
+	}
+
+}
